28234dc4f4346c2dbfc748c02f0c49b63e559e32
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2023-05-09 10:35:37 -0500

    Adding splash screen

--- android/app/src/main/java/com/rntemplate/MainActivity.java
+++ android/app/src/main/java/com/rntemplate/MainActivity.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -38,5 +40,6 @@ public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SplashScreen.show(this, R.style.AppTheme, false);
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   NavigationContainer,
   useNavigationContainerRef,
 } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFlipper } from "@react-navigation/devtools";
+import SplashScreen from "react-native-splash-screen";
 
 import HomeScreen from "./screens/home";
 import { routes } from "./routes";
@@ -13,8 +14,29 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   const navigationRef = useNavigationContainerRef();
+  const [isSplashScreen, setIsSplashScreen] = useState(true);
 
   useFlipper(navigationRef);
+
+  useEffect(() => {
+    async function initialize() {
+      try {
+        // Set up or get initial data, for example: get user info or request some permissions
+        setIsSplashScreen(false);
+      } catch (error) {}
+    }
+    initialize();
+  }, []);
+
+  useEffect(() => {
+    if (!isSplashScreen) {
+      SplashScreen.hide();
+    }
+  }, [isSplashScreen]);
+
+  if (isSplashScreen) {
+    return null;
+  }
 
   return (
     <NavigationContainer ref={navigationRef}>
