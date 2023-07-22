0f45807bdbd3b0a7484948005ab88ded789897b6
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2023-07-21 23:55:31 -0500

    Hiding SSRProvider warning

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ import { AppRegistry, LogBox } from "react-native";
 import App from "./src/App";
 import { name as appName } from "./app.json";
 
-// TODO: Fix this warn
+// TODO: Fix these warnings
 LogBox.ignoreLogs([
   "No task registered for key ReactNativeFirebaseMessagingHeadlessTask",
+  "In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app",
 ]);
 
 function HeadlessCheck({ isHeadless }) {
