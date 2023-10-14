475036e699436d3fed90c1ecc866d9e3e7937a45
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2023-10-14 11:47:57 -0500

    Patch for native base

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
       require.resolve("babel-plugin-module-resolver"),
       {
         cwd: "babelrc",
-        extensions: [".ts", ".tsx", ".js", ".jsx", ".ios.js", ".android.js"],
+        extensions: [".ts", ".tsx", ".js", ".ios.js", ".android.js"],
         alias: {
           "@": "./src",
         },
